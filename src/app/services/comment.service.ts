be4b82b0d6f029725639700f1b4d245746298c4b
Axielles <awacks@gmail.com>
2019-01-18 13:18:53 +0100

    add POST PUT DELETE méthods in Comment service

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -7,6 +7,7 @@ import { HttpClient } from '@angular/common/http';
 
 export class CommentService {
   URL = 'http://localhost:8080/api/comments';
+  comment: Comment;
 
   constructor(private http: HttpClient) { }
 
@@ -14,4 +15,25 @@ export class CommentService {
     return this.http.get(`${this.URL}`);
   }
 
+  public getCommentById(id: number) {
+    return this.http.get(`${this.URL}/${id}`);
+  }
+
+  public postComment() {
+    return this.http.post(`${this.URL}`, this.comment);
+  }
+
+  public putComment(id: number) {
+    return this.http.put(`${this.URL}/${id}`, this.comment);
+  }
+
+  public deleteComment(id: number) {
+    return this.http.delete(`${this.URL}/${id}`);
+  }
+
+}
+
+class Comment {
+  content: String;
+  liky = 0;
 }
