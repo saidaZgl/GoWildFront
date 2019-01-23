1867d4407fee4d77d00fa2bda2598df227146272
Axielles <awacks@gmail.com>
2019-01-23 12:26:36 +0100

    display comment

--- src/app/comments/comment/comment.component.ts
+++ src/app/comments/comment/comment.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Comment } from '../../class/comment';
+import { Component, OnInit } from '@angular/core';
+import { Comment } from '../../models/comment';
+import { CommentService } from '../../services/comment.service';
 
 @Component({
   selector: 'app-comment',
@@ -7,8 +8,19 @@ import { Comment } from '../../class/comment';
   styleUrls: ['./comment.component.css']
 })
 
-export class CommentComponent {
+export class CommentComponent implements OnInit {
+  public commentModel: Comment;
 
-  commentModel = new Comment(1, 'Ceci est un commentaire sur un événement', 7);
+  constructor ( private commentService: CommentService ) {
+  }
+
+  ngOnInit() {
+      this.commentModel = new Comment(1, 'Ceci est un commentaire sur un événement', 7);
+      /* this.commentService.getCommentById(2).subscribe((response: any) => {
+      this.commentModel = response;
+      console.log(this.commentModel);
+    });
+    */
+  }
 
 }
